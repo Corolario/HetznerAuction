92ab1e891ff0e300f10ba06623ba08a294594bd7
Corolário <21142386+Corolario@users.noreply.github.com>
2024-06-04 12:15:49 -0300

    Update scriptReboot.bat

--- scriptReboot.bat
+++ scriptReboot.bat
@@ -3,6 +3,20 @@ if errorlevel 1 (
 	timeout /t 30
     	ping -n 10 8.8.8.8 | find "TTL="
 	if errorlevel 1 (
-    		start shutdown /r /t 00
+    		wmic path win32_networkadapter where index=1 call disable
+		timeout /t 10
+		wmic path win32_networkadapter where index=1 call enable
+		timeout /t 30
+		ping -n 10 1.1.1.1 | find "TTL="
+		if errorlevel 1 (
+			timeout /t 30
+		    	ping -n 10 8.8.8.8 | find "TTL="
+			if errorlevel 1 (
+		    		start shutdown /r /t 00
+			)
+		)
 	)
 )
+
+
+		
